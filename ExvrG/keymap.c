c5b282f3d27195fdde698a73facd4644cc31cd62
csravelar <csravelar@gmail.com>
2025-01-13 15:34:41 -0600

    Update

--- ExvrG/keymap.c
+++ ExvrG/keymap.c
@@ -17,19 +17,17 @@ void housekeeping_task_user(void) {
   achordion_task();
 }
 
-
 enum tap_dance_codes {
   DANCE_0,
   DANCE_1,
 };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TO(1),                                          TO(2),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TO(2),                                          TO(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TD(DANCE_0),                                    KC_EQUAL,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_CAPS,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_ESCAPE,                                                                      KC_COLN,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
     KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
-    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_AUDIO_MUTE,  KC_LEFT,        KC_RIGHT,       KC_LEFT_SHIFT,                                                                                                  KC_RIGHT_CTRL,  KC_UP,          KC_DOWN,        KC_LBRC,        KC_RBRC,        KC_RIGHT_CTRL,  
+    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_LEFT,        KC_RIGHT,       KC_LEFT_SHIFT,                                                                                                  KC_RIGHT_CTRL,  KC_UP,          KC_DOWN,        KC_LBRC,        KC_RBRC,        KC_RIGHT_CTRL,  
     KC_SPACE,       KC_BSPC,        KC_LEFT_GUI,                    KC_ESCAPE,      KC_TAB,         KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
@@ -41,15 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_VAD,        RGB_VAI,        TOGGLE_LAYER_COLOR,                RGB_SLD,        RGB_HUD,        RGB_HUI
   ),
   [2] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TRANSPARENT, DM_REC1,        DM_RSTP,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TRANSPARENT, DM_PLY1,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TO(0),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
-
 const uint16_t PROGMEM combo0[] = { KC_F1, KC_F2, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
@@ -106,81 +103,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
     case ST_MACRO_0:
-      if (record->event.pressed) {
-        SEND_STRING(SS_TAP(X_DOWN) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_DOWN));
-      }
-      break;
+    if (record->event.pressed) {
+      SEND_STRING(SS_TAP(X_DOWN) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_DOWN));
+    }
+    break;
 
     case RGB_SLD:
-      if (rawhid_state.rgb_control) {
+        if (rawhid_state.rgb_control) {
+            return false;
+        }
+        if (record->event.pressed) {
+            rgblight_mode(1);
+        }
         return false;
-      }
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-
-    case MACRO_Q:
-      if (record->event.pressed) {
-        register_code(KC_LCTL);
-        tap_code_delay(KC_W, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_S, 100);
-        unregister_code(KC_LCTL);
-      }
-      break;
-
-    case MACRO_W:
-      if (record->event.pressed) {
-        register_code(KC_LCTL);
-        tap_code_delay(KC_W, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_D, 100);
-        unregister_code(KC_LCTL);
-      }
-      break;
-
-    case MACRO_E:
-      if (record->event.pressed) {
-        register_code(KC_LCTL);
-        tap_code_delay(KC_W, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_W, 100);
-        unregister_code(KC_LCTL);
-      }
-      break;
-
-    case MACRO_R:
-      if (record->event.pressed) {
-        register_code(KC_LCTL);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_A, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_W, 100);
-        unregister_code(KC_LCTL);
-      }
-      break;
-
-    case MACRO_T:
-      if (record->event.pressed) {
-        register_code(KC_LCTL);
-        tap_code_delay(KC_S, 100);
-        tap_code_delay(KC_W, 100);
-        tap_code_delay(KC_D, 100);
-        tap_code_delay(KC_A, 100);
-        unregister_code(KC_LCTL);
-      }
-      break;
+    case HSV_0_255_255:
+        if (rawhid_state.rgb_control) {
+            return false;
+        }
+        if (record->event.pressed) {
+            rgblight_mode(1);
+            rgblight_sethsv(0,255,255);
+        }
+        return false;
+    case HSV_86_255_128:
+        if (rawhid_state.rgb_control) {
+            return false;
+        }
+        if (record->event.pressed) {
+            rgblight_mode(1);
+            rgblight_sethsv(86,255,128);
+        }
+        return false;
+    case HSV_172_255_255:
+        if (rawhid_state.rgb_control) {
+            return false;
+        }
+        if (record->event.pressed) {
+            rgblight_mode(1);
+            rgblight_sethsv(172,255,255);
+        }
+        return false;
   }
   return true;
 }
+
 
 typedef struct {
     bool is_press_action;
@@ -288,4 +254,3 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
 };
-

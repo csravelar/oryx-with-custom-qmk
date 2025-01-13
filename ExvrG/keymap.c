639d802853665e828bdb2f65bcf51bb6132ef2a7
csravelar <csravelar@gmail.com>
2025-01-13 16:55:17 -0600

    Update

--- ExvrG/keymap.c
+++ ExvrG/keymap.c
@@ -7,6 +7,7 @@
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   CTRL_WDSA,
+  KG_BOMB,
   HSV_0_255_255,
   HSV_86_255_128,
   HSV_172_255_255,
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [2] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TO(0),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TRANSPARENT, CTRL_WDSA,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
+    KC_TRANSPARENT, CTRL_WDSA,           KG_BOMB,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -145,14 +146,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     case CTRL_WDSA:
 	if (record->event.pressed) {
-            register_code(KC_LCTL);
-	    tap_code(KC_W);
-   	    tap_code(KC_D);
-	    tap_code(KC_S);
-            tap_code(KC_A);
-            unregister_code(KC_LCTL);
+	    tap_code(KC_ENTER);
+            wait_ms(100);
+            SEND_STRING("hello world");
+            wait_ms(100);
+            tap_code(KC_ENTER);
         }
         return false; 
+    case KG_BOMB:
+        if (record->event.pressed) {
+            register_code(KC_LCTL);
+	    wait_ms(100);
+	    tap_code_delay(KC_W, 100);
+	    tap_code_delay(KC_D, 100);
+	    tap_code_delay(KC_S, 100);
+	    tap_code_delay(KC_A, 100);
+	    wait_ms(100);
+	    unregister_code(KC_LCTL);
+    }
+    return false;
   }
   return true;
 }
